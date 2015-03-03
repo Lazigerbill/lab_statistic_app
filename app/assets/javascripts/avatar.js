2a1891281c7be6e1181b0b0ac86dab8828df781b
Lazigerbill <chaukbill@gmail.com>
2015-03-03 16:32:52 -0500

    added preview to paperclip

---
+++ app/assets/javascripts/avatar.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('#pic-upload').on('change', function(event) {
+    var files = event.target.files;
+    var image = files[0]
+    var reader = new FileReader();
+    reader.onload = function(file) {
+      var img = new Image();
+      img.src = file.target.result;
+      img.height = 200;
+      console.log(img);
+      $('#preview').html(img);
+    }
+    reader.readAsDataURL(image);
+    console.log(files);
+  });
+});
