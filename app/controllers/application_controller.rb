19ad98a789251f2e0af03447efccfe876a132242
Lazigerbill <chaukbill@gmail.com>
2015-03-04 16:08:48 -0500

    updated redirect path after sign in/out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  
+
 
 
   protected
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource)
+    new_entry_path
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar) }
